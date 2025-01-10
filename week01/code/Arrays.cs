071ee3d723d618dc7f0e9015708087cb74365c70
santichapa <santi.chaparro280@gmail.com>
2025-01-10 17:00:01 -0300

    finished arrays activity

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,17 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // first, create an array of doubles with the same length as the parameter.
+        // then, use a for loop  to iterate through the array and set the value in each index to the product of the number parameter and the index.
+        // then, return the new array.
+
+        var multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +39,27 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // first, create a new list to store the values at the end that will be swapped to the beginning of the list.
+        // then, use a for loop to iterate through the list and set the value of the starting index to the value of the last index minus the amount parameter. 
+        // Inside the loop, add the changing numbers to the temporary list.
+        // then, use a for loop to iterate through the temporary list and set the value at the index to the value at the index plus the amount parameter.
+        // then, assing the values in the temporary list to the beginning of the list, the same index is used for both so the numbers will be placed at the beginning of the list.
+        // you don't need to return anything because the list is being modified in place.
+
+        var temp = new List<int>();
+
+        for (int i = data.Count - amount; i < data.Count; i++)
+        {
+            temp.Add(data[i]);
+        }
+        for (int i = data.Count - amount - 1; i >= 0; i--)
+        {
+            data[i + amount] = data[i];
+        }
+        for (int i = 0; i < amount; i++)
+        {
+            data[i] = temp[i];
+        }
     }
 }
