f371d33d82defdcf30fa498248536530cf398f6d
santichapa <santi.chaparro280@gmail.com>
2025-01-17 14:37:29 -0300

    finished team activities

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -11,24 +11,79 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 1");
-
-        // Defect(s) Found: 
+        // Scenario: if max size is <+ 0
+        // Expected Result: max size reverts to 10
+        // Console.WriteLine("Test 1");
+        // var cs = new CustomerService(-1);
+        // Console.WriteLine(cs);
+ 
+        // Defect(s) Found: No Defect
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
-        Console.WriteLine("Test 2");
+        // Scenario: The AddNewCustomer method shall enqueue a new customer into the queue.
+        // Expected Result: Customer added successfully.
+        // var cs = new CustomerService(10);
+        // Console.WriteLine("Test 2");
+        // cs.AddNewCustomer();
+        // cs.AddNewCustomer();
+        // Console.WriteLine(cs);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: No defect found.
 
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+
+        // Test 3   
+        // Scenario: the queue is full and a new customer is added
+        // expected result: error message displayed
+        // var cs = new CustomerService(1);
+        // Console.WriteLine("Test 3");
+        // cs.AddNewCustomer();
+        // cs.AddNewCustomer();
+        // cs.AddNewCustomer();
+        // Console.WriteLine(cs);
+
+        // Defect(s) Found: the program let me add an extra person to the queue before showing an error
+
+        // Test 4
+
+        // Scenario: Removing a customer from the queue
+        // Expected Result: Removes a customer from the queue if there is one or prints "No Customers in queue"
+        // Defect(s) Found: 2
+        // Console.WriteLine("\nTest 4");
+
+        // // Initialize the Service
+        // CustomerService customerService = new(4);
+
+        // // Test Remove from queue with empty queue
+        // Console.WriteLine("Expected:  No Customers in the queue");
+        // Console.Write("Actual: ");
+        // customerService.ServeCustomer();
+
+        // // Add a customer
+        // Console.WriteLine();
+        // customerService.AddNewCustomer();
+
+
+        // // Test Remove from queue with one customer
+        // Console.WriteLine("\nExpected: This should display the customer that was added");
+        // Console.Write("Actual: ");
+        // customerService.ServeCustomer();
+
+        // // Finish Test 4
+        // Console.WriteLine(new string('=', 17));
+
+        // // Test 5
+        // // Scenario: The ServeCustomer method is called when the queue is empty
+        // // Expected Result: Error message (Index out of range error.)
+        // Console.WriteLine("Test 5");
+        // cs.AddNewCustomer();
+        // cs.ServeCustomer();
+        // cs.ServeCustomer();
+        // Console.WriteLine(cs);
     }
 
     private readonly List<Customer> _queue = new();
@@ -38,7 +93,7 @@ public class CustomerService {
         if (maxSize <= 0)
             _maxSize = 10;
         else
-            _maxSize = maxSize;
+            _maxSize = maxSize - 1; // reduce the maxSize by one to reflect the input instead of the index
     }
 
     /// <summary>
