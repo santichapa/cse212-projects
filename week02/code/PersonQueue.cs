d42d9d71fd4ce5e297b294c52f29abba49582dd9
santichapa <santi.chaparro280@gmail.com>
2025-01-17 22:18:28 -0300

    Finished problem 1 week02

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -13,7 +13,7 @@ public class PersonQueue
     /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Insert(0, person);
+        _queue.Add(person); // defect 1: people were added to the start of the queue
     }
 
     public Person Dequeue()
